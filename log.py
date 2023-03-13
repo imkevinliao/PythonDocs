247dfcbf4e70a96fabe39870090f973ff8e96a54
imkevinliao <imkevinliao@gmail.com>
2023-03-13 16:01:07 +0800

    init

---
+++ log.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+
+class Log(logging.Logger):
+    def __init__(self, log_filepath=None, level=logging.DEBUG, logfile_mode='a+'):
+        super().__init__(name=log_filepath)
+        self.logfile_mode = logfile_mode
+        self.setLevel(level=level)
+        self.config()
+    
+    def config(self):
+        formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s [line:%(lineno)d] %(message)s",
+                                      datefmt="%Y-%m-%d %H:%M:%S(%p)")
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(formatter)
+        self.addHandler(stdout_handler)
+        if self.name:
+            file_handler = logging.FileHandler(filename=self.name, encoding='utf-8', mode=self.logfile_mode)
+            file_handler.setFormatter(formatter)
+            self.addHandler(file_handler)
