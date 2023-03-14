22ff0157f442c8cb7f1d7ebb790c5fd8c4e6e358
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-03-14 11:44:00 +0800

    Update util.py

--- util.py
+++ util.py
@@ -3,9 +3,10 @@ import sys
 
 def decode(decoded_string: str, encoding_set=None) -> str:
     """
-    decoded_string:待解码的字符串
-    encoding_set:指定解码的字符集
-    return: 解码失败则返回原字符串，解码成功则返回解码后的字符串
+    decoded_string: 待解码的字符串(bytes)
+    encoding_set: 指定解码的字符集
+    return: str [解码成功返回解码后字符串，解码失败返回原来的字符串]
+    
     注意：解码成功并不代表正确（如下面的字符串，正确解码是 gbk 可阅读，但是 "utf-16" 同样可以解码成功，不可阅读.
     r = b"'sudo' \xb2\xbb\xca\xc7\xc4\xda\xb2\xbf\xbb\xf2\xcd\xe2\xb2\xbf\xc3\xfc\xc1\xee\xa3\xac\xd2\xb2\xb2\xbb\xca\xc7" \
     b"\xbf\xc9\xd4\xcb\xd0\xd0\xb5\xc4\xb3\xcc\xd0\xf2\r\n\xbb\xf2\xc5\xfa\xb4\xa6\xc0\xed\xce\xc4\xbc\xfe\xa1\xa3\r\n "
