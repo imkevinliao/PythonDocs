51c70ff185f9b90934cef6acb4fb5c40f7bc3359
KevinLawrence <87458144+imkevinliao@users.noreply.github.com>
2024-07-05 15:25:16 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if command -v python3 &>/dev/null; then
+    :
+else
+    echo "installing Python3"
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$NAME
+    else
+        echo "Error: Unsupported OS"
+        exit 1
+    fi
+    case $OS in
+        "Ubuntu" | "Debian GNU/Linux")
+            sudo apt update
+            sudo apt install python3 -y
+            ;;
+        "CentOS Linux" | "Red Hat Enterprise Linux")
+            sudo yum update
+            sudo yum install python3 -y
+            ;;
+        *)
+            echo "Error: Unsupported OS"
+            exit 1
+            ;;
+    esac
+fi
+
+# 执行python脚本
+curl -sSL https://raw.githubusercontent.com/imkevinliao/[project name]/[project branch]/[python script] | python3
